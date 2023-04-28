ce491f17c87c5fb239b838358b163555d819cbc6
Matt <matt.atkins216@gmail.com>
2023-04-27 21:14:36 -0600

    Revert "dashboard layout is done, working on form to add grow"
    This reverts commit 20b4feaa7a161117dfdbf3e837089cd0789bad6d.

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 // DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false`
 // by default in Mongoose 7.
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/myapp" // < replace with your database name!
+	'mongodb://127.0.0.1:27017/myapp' // < replace with your database name!
 );
 
 const db = mongoose.connection;
 
-db.on("connected", function () {
+db.on('connected', function() {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
 
-db.on("error", (err) => {
-  console.log(err);
+db.on('error', err => {
+	console.log(err);
 });
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { configureStore } from '@reduxjs/toolkit'
-import globalReducer from "./state"
-import { Provider } from 'react-redux'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
